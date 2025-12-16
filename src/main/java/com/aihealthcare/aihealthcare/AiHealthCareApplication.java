6826d87f3cfb2e76c1324790fbd6ba5c995ef0e2
jaeyoonjung(rexyoon) <137620925+rexyoon@users.noreply.github.com>
2025-12-16 22:35:33 +0900

    setting alright

---
+++ src/main/java/com/aihealthcare/aihealthcare/AiHealthCareApplication.java
@@ -0,0 +1,13 @@
+package com.aihealthcare.aihealthcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiHealthCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiHealthCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aihealthcare/aihealthcare/AiHealthCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aihealthcare.aihealthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiHealthCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
