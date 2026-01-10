4c969f25b7800313bdf9536e9fc6297a42e44898
jaeyoonjung(rexyoon) <137620925+rexyoon@users.noreply.github.com>
2026-01-10 22:50:02 +0900

    feat image to text

---
+++ src/main/java/com/aihealthcare/aihealthcare/util/ImageToText.java
@@ -0,0 +1,22 @@
+//package com.aihealthcare.aihealthcare.utils;
+//
+//import net.sourceforge.tess4j.Tesseract;
+//import net.sourceforge.tess4j.TesseractException;
+//
+//import java.io.File;
+//
+//public class ImageToText {
+//    public static String extractTextFromImage(String imagePath) {
+//        Tesseract tesseract = new Tesseract();
+//        tesseract.setDatapath("src/main/resources/tessdata"); // Tesseract 데이터 경로 설정
+//        tesseract.setLanguage("eng"); // 언어 설정
+//
+//        try {
+//            return tesseract.doOCR(new File(imagePath));
+//        } catch (TesseractException e) {
+//            e.printStackTrace();
+//            return null;
+//        }
+//    }
+//}
+////
