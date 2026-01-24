9a9b4109f6ad8f70daa5708d8328e57578a611e4
jaeyoonjung(rexyoon) <137620925+rexyoon@users.noreply.github.com>
2026-01-24 15:03:49 +0900

    fix application.yml

--- src/main/java/com/aihealthcare/aihealthcare/repository/UserRepository.java
+++ src/main/java/com/aihealthcare/aihealthcare/repository/UserRepository.java
@@ -3,9 +3,5 @@ package com.aihealthcare.aihealthcare.repository;
 import com.aihealthcare.aihealthcare.domain.user.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
- Optional<UserEntity> findByEmail(String email);
- boolean existsByEmail(String email);
-}
+ }
